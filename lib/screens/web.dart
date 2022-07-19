4420233eae25bb78dd8a8f05c282c5164016be8f
Umar-Waseem <umar.waseem@gmail.com>
2022-07-19 20:27:29 +0500

    contacts for mobile

--- lib/screens/web.dart
+++ lib/screens/web.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/contacts.dart';
+
 class WebScreen extends StatelessWidget {
   const WebScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Web")),
+      body: Contacts(),
     );
   }
 }
--- lib/widgets/contacts.dart
+++ lib/widgets/contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../colors.dart';
 import 'dummy.dart';
 
 class Contacts extends StatelessWidget {
@@ -9,32 +10,48 @@ class Contacts extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: ListView.builder(
-        itemCount: info.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              info[index]["name"].toString(),
-              style: const TextStyle(fontSize: 18),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: info.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: ListTile(
+                      title: Text(
+                        info[index]["name"].toString(),
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          info[index]["message"].toString(),
+                          style: const TextStyle(fontSize: 15),
+                        ),
+                      ),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                          info[index]["profilePic"].toString(),
+                        ),
+                      ),
+                      trailing: Text(
+                        info[index]["time"].toString(),
+                        style:
+                            const TextStyle(fontSize: 15, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: Text(
-                info[index]["message"].toString(),
-                style: const TextStyle(fontSize: 15),
-              ),
-            ),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                info[index]["profilePic"].toString(),
-              ),
-            ),
-            trailing: Text(
-              info[index]["time"].toString(),
-              style: const TextStyle(fontSize: 15, color: Colors.grey),
-            ),
-          );
-        },
+          ),
+          const Divider(color: dividerColor, indent: 85),
+        ],
       ),
     );
   }
